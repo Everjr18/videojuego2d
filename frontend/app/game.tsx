a7fbfc17a0a0dec727a3a2e03c143c134533dbcd
diegonicita <diegonicita@gmail.com>
2024-09-02 13:24:50 -0300

    Refactor game component

--- frontend/app/game.tsx
+++ frontend/app/game.tsx
@@ -34,41 +34,50 @@ export default function Game() {
   return (
     <div className="flex justity-center bg-blue-300">
       {isMounted && (
-        <div className="flex">
-          <div className="mt-2 px-4 flex gap-2 w-60 text-black">
-            <span>Height: {screenHeight}</span>{' '}
-            <span>Width: {screenWidth}</span>
+        <div className="flex justify-center w-full my-20">
+          <div className="mt-2 px-4 w-60 flex flex-col text-2xl gap-2 text-black">
+            <span>Level 1</span>
+            <span>Country: Argentina </span>            
           </div>
-          <Music />
-          <Stage
-            width={screenWidth}
-            height={screenHeight}
-            options={{ background: 0x1099bb }}
-          >
-            <EnemyController />
-            <HeroController />
+          <div>
+            <div className="text-black text-2xl mb-4">COVID-19: Fuera de Control </div>
+            <Music />
+            <Stage
+              width={screenWidth}
+              height={screenHeight}
+              options={{ background: 0x1099bb }}
+            >
+              <EnemyController />
+              <HeroController />
 
-            <Container x={150} y={150}>
-              <Text
-                text={'Fuera de Control'}
-                anchor={0.5}
-                x={400}
-                y={400}
-                filters={[]}
-                style={
-                  new TextStyle({
-                    align: 'center',
-                    fill: '0xffffff',
-                    fontSize: 24,
-                    letterSpacing: 10,
-                    dropShadow: true,
-                    dropShadowColor: '#E72264',
-                    dropShadowDistance: 6,
-                  })
-                }
-              />
-            </Container>
-          </Stage>
+              <Container x={150} y={150}>
+                <Text
+                  text={'Fuera de Control'}
+                  anchor={0.5}
+                  x={400}
+                  y={400}
+                  filters={[]}
+                  style={
+                    new TextStyle({
+                      align: 'center',
+                      fill: '0xffffff',
+                      fontSize: 24,
+                      letterSpacing: 10,
+                      dropShadow: true,
+                      dropShadowColor: '#E72264',
+                      dropShadowDistance: 6,
+                    })
+                  }
+                />
+              </Container>
+            </Stage>
+          </div>
+          <div className="mt-2 px-4 flex flex-col gap-2 text-black text-2xl">
+            <span>Score: {screenHeight}</span>{' '}
+            <span>Cured: {screenWidth}</span>
+            <span>Infected: {screenWidth}</span>
+            <span>Deads: {screenWidth}</span>
+          </div>
         </div>
       )}
     </div>
